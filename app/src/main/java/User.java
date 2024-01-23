3f43a9bce846c47b8d94aefb809f39d3ff4ec679
Omar EL HAMDAOUI <omar.EL-HAMDAOUI@etu.univ-amu.fr>
2024-01-23 23:58:09 +0100

    class User constructeur

--- app/src/main/java/User.java
+++ app/src/main/java/User.java
@@ -1,2 +1,12 @@
 public class User {
+    private final int id;
+    private final String Name;
+    private static int counter=0;
+
+    public User(int id, String name) {
+        this.id = ++counter;
+        Name = name;
+    }
+
+
 }
