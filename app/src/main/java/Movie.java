b63a49441182d590885c99362b3f6fc309e56b98
Omar EL HAMDAOUI <omar.EL-HAMDAOUI@etu.univ-amu.fr>
2024-01-24 00:29:06 +0100

    class Movie constructeur

--- app/src/main/java/Movie.java
+++ app/src/main/java/Movie.java
@@ -1,2 +1,15 @@
 public class Movie {
+    private final String name;
+    private final int dateOfRelease;
+    private int note;
+    private final String kind;
+
+    public Movie(String name,int dateOfRealease,int note,String kind) {
+        this.name = name;
+        this.dateOfRelease=dateOfRealease;
+        this.note=note;
+        this.kind=kind;
+    }
+
+
 }
