567b4e6bd525ba5e4b6d6fa545400509ffdc0525
Omar EL HAMDAOUI <omar.EL-HAMDAOUI@etu.univ-amu.fr>
2024-01-24 00:06:29 +0100

    class User getName(),getId()

--- app/src/main/java/User.java
+++ app/src/main/java/User.java
@@ -1,12 +1,17 @@
 public class User {
     private final int id;
-    private final String Name;
+    private final String name;
     private static int counter=0;
 
     public User(int id, String name) {
         this.id = ++counter;
-        Name = name;
+        this.name = name;
     }
-
+    public String getName(){
+        return name;
+    }
+    public int getId(){
+        return id;
+    }
 
 }
