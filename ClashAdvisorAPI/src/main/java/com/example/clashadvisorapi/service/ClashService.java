058abf3d401d795e5adf27927bfe4a27c2df1c44
omar <omarelhamdaoui39@gmail.com>
2025-04-06 12:45:16 +0200

    Update dependecies on build.gradle

--- ClashAdvisorAPI/src/main/java/com/example/clashadvisorapi/service/ClashService.java
+++ ClashAdvisorAPI/src/main/java/com/example/clashadvisorapi/service/ClashService.java
@@ -2,13 +2,17 @@ package com.example.clashadvisorapi.service;
 
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
+
 
 @Service
 public class ClashService {
