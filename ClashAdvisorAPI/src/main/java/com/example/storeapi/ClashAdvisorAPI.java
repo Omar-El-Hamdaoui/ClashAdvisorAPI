4b3b88ad369f4c751bb8592b884dfa6a5aa07898
omar <omarelhamdaoui39@gmail.com>
2025-04-06 11:53:06 +0200

    Rename Project name to ClashAdvisorAPI

---
+++ ClashAdvisorAPI/src/main/java/com/example/storeapi/ClashAdvisorAPI.java
@@ -0,0 +1,14 @@
+package com.example.storeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClashAdvisorAPI {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClashAdvisorAPI.class, args);
+		System.out.println("New Begin");
+	}
+
+}
