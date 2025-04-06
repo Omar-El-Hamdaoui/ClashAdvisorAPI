5d38e47c30b8f906a9cc05e5000f2189799c8b8b
omar <omarelhamdaoui39@gmail.com>
2025-04-06 13:02:02 +0200

    Création Controller

--- ClashAdvisorAPI/src/main/java/com/example/clashadvisorapi/ClashAdvisorAPI.java
+++ ClashAdvisorAPI/src/main/java/com/example/clashadvisorapi/ClashAdvisorAPI.java
@@ -2,6 +2,7 @@ package com.example.clashadvisorapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ClashAdvisorAPI {
@@ -9,6 +10,8 @@ public class ClashAdvisorAPI {
 	public static void main(String[] args) {
 		SpringApplication.run(ClashAdvisorAPI.class, args);
 		System.out.println("New Begin");
+		RestTemplate restTemplate = new RestTemplate();
+		System.out.println("RestTemplate OK ✅");
 	}
 
 }
