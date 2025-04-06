919b437e76753037a4c571c5a868215ab95797b0
omar <omarelhamdaoui39@gmail.com>
2025-04-06 13:07:31 +0200

    Implement VillageController

--- ClashAdvisorAPI/src/main/java/com/example/clashadvisorapi/controller/VillageController.java
+++ ClashAdvisorAPI/src/main/java/com/example/clashadvisorapi/controller/VillageController.java
@@ -1,4 +1,23 @@
 package com.example.clashadvisorapi.controller;
 
+import com.example.clashadvisorapi.service.ClashService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/village")
 public class VillageController {
+
+    private final ClashService clashService;
+
+    public VillageController(ClashService clashService) {
+        this.clashService = clashService;
+    }
+
+    @GetMapping("/info/{tag}")
+    public ResponseEntity<?> getVillageInfo(@PathVariable String tag) {
+        return ResponseEntity.ok(clashService.getPlayerInfo(tag));
+    }
 }
+
